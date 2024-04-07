8ad51aca53c88ce4cb26251cc861f398cdb94f78
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-04-07 15:18:59 -0700

    refactor: changed cmp to use tab for confirming, and up and down for doc scrolling

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -12,11 +12,34 @@ return {
   },
 
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        width = 35,
-      },
-    },
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require "cmp"
+      local luasnip = require "luasnip"
+      opts.mapping = {
+        ["<Up>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+        ["<Down>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+        ["<CR>"] = cmp.mapping.confirm { select = false },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm { select = true }
+          elseif luasnip.jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }
+    end,
+    dependencies = "L3MON4D3/LuaSnip",
   },
 }
