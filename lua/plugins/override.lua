e6ae25b8a02ef5b0d90c61dd0b4a555b1586089e
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-04-13 06:41:34 -0700

    chore: consolidated config

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -10,36 +10,27 @@ return {
       },
     },
   },
-
   {
-    "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
-    dependencies = {
-      { "AstroNvim/astroui", opts = { icons = { Trouble = "󱍼" } } },
-      {
-        "AstroNvim/astrocore",
-        opts = function(_, opts)
-          local maps = opts.mappings
-          local prefix = "<Leader>x"
-          maps.n[prefix] = { desc = require("astroui").get_icon("Trouble", 1, true) .. "Trouble" }
-          maps.n[prefix .. "x"] = { "<Cmd>TroubleToggle document_diagnostics<CR>", desc = "Document Diagnostics (Trouble)" }
-          maps.n[prefix .. "X"] = { "<Cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Workspace Diagnostics (Trouble)" }
-          maps.n[prefix .. "r"] = { "<Cmd>TroubleToggle lsp_references<CR>", desc = "LSP References (Trouble)" }
-          maps.n[prefix .. "d"] = { "<Cmd>TroubleToggle lsp_definitions<CR>", desc = "LSP Definitions (Trouble)" }
-          maps.n[prefix .. "t"] = { "<Cmd>TroubleToggle lsp_type_definitions<CR>", desc = "LSP Type Definitions (Trouble)" }
-          maps.n[prefix .. "i"] = { "<Cmd>TroubleToggle lsp_implementations<CR>", desc = "LSP Implementations (Trouble)" }
-        end,
-      },
-    },
-    opts = {
-      use_diagnostic_signs = true,
-      action_keys = {
-        close = { "q", "<ESC>" },
-        cancel = "<C-e>",
-      },
-    },
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "bash",
+        "diff",
+        "json",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "toml",
+        "vim",
+        "vimdoc",
+      })
+    end,
   },
-
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,7 +6,6 @@ return {
 
   -- Plugins
   { "folke/tokyonight.nvim" },
-
   {
     "mbbill/undotree",
     config = function()
@@ -16,7 +15,6 @@ return {
       vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle)
     end,
   },
-
   {
     "christoomey/vim-tmux-navigator",
     keys = {
@@ -24,6 +22,34 @@ return {
       { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
       { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
       { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    dependencies = {
+      { "AstroNvim/astroui", opts = { icons = { Trouble = "󱍼" } } },
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          local prefix = "<Leader>x"
+          maps.n[prefix] = { desc = require("astroui").get_icon("Trouble", 1, true) .. "Trouble" }
+          maps.n[prefix .. "x"] = { "<Cmd>TroubleToggle document_diagnostics<CR>", desc = "Document Diagnostics (Trouble)" }
+          maps.n[prefix .. "X"] = { "<Cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Workspace Diagnostics (Trouble)" }
+          maps.n[prefix .. "r"] = { "<Cmd>TroubleToggle lsp_references<CR>", desc = "LSP References (Trouble)" }
+          maps.n[prefix .. "d"] = { "<Cmd>TroubleToggle lsp_definitions<CR>", desc = "LSP Definitions (Trouble)" }
+          maps.n[prefix .. "t"] = { "<Cmd>TroubleToggle lsp_type_definitions<CR>", desc = "LSP Type Definitions (Trouble)" }
+          maps.n[prefix .. "i"] = { "<Cmd>TroubleToggle lsp_implementations<CR>", desc = "LSP Implementations (Trouble)" }
+        end,
+      },
+    },
+    opts = {
+      use_diagnostic_signs = true,
+      action_keys = {
+        close = { "q", "<ESC>" },
+        cancel = "<C-e>",
+      },
     },
   },
 }
