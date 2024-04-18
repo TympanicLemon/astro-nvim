58adde7fbed2f39cf28c41820b222ba8bb546939
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-04-18 13:40:36 -0700

    refactor: changed trouble keymaps

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -47,16 +47,10 @@ return {
           local maps = opts.mappings
           local prefix = "<Leader>x"
           maps.n[prefix] = { desc = require("astroui").get_icon("Trouble", 1, true) .. "Trouble" }
-          maps.n[prefix .. "x"] =
-            { "<Cmd>TroubleToggle document_diagnostics<CR>", desc = "Document Diagnostics (Trouble)" }
-          maps.n[prefix .. "X"] =
-            { "<Cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Workspace Diagnostics (Trouble)" }
-          maps.n[prefix .. "r"] = { "<Cmd>TroubleToggle lsp_references<CR>", desc = "LSP References (Trouble)" }
-          maps.n[prefix .. "d"] = { "<Cmd>TroubleToggle lsp_definitions<CR>", desc = "LSP Definitions (Trouble)" }
-          maps.n[prefix .. "t"] =
-            { "<Cmd>TroubleToggle lsp_type_definitions<CR>", desc = "LSP Type Definitions (Trouble)" }
-          maps.n[prefix .. "i"] =
-            { "<Cmd>TroubleToggle lsp_implementations<CR>", desc = "LSP Implementations (Trouble)" }
+          maps.n[prefix .. "x"] = { "<Cmd>TroubleToggle document_diagnostics<CR>", desc = "Document Diagnostics (Trouble)" }
+          maps.n[prefix .. "X"] = { "<Cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Workspace Diagnostics (Trouble)" }
+          maps.n[prefix .. "l"] = { "<Cmd>TroubleToggle loclist<CR>", desc = "Location List (Trouble)" }
+          maps.n[prefix .. "q"] = { "<Cmd>TroubleToggle quickfix<CR>", desc = "Quickfix List (Trouble)" }
         end,
       },
     },
