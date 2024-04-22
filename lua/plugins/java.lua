447e677457dff30e55fb42117a1179f5dcab61df
TympanicLemon <danielz@Daniels-MBP.attlocal.net>
2024-04-22 13:06:31 -0700

    feat: added xml and yaml to java config

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -4,7 +4,8 @@ return {
     optional = true,
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "java", "html" })
+        opts.ensure_installed =
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "java", "html", "xml", "yaml" })
       end
     end,
   },
