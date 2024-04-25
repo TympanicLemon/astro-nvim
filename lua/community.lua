8c66ca6bf3f251400f9d52c56363caf7a412f577
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-04-24 18:57:48 -0700

    refactor: cleaned up config a ton

--- lua/community.lua
+++ lua/community.lua
@@ -1,10 +1,12 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.utility.mason-tool-installer-nvim" },
 }
