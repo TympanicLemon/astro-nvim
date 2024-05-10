3eea905ecf04031d7947466a5dad3f8e1fee1632
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-05-09 20:11:53 -0700

    feat: added clang support & updated lazy-lock

--- lua/community.lua
+++ lua/community.lua
@@ -5,8 +5,8 @@ return {
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.lua" },
-  -- { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.utility.mason-tool-installer-nvim" },
 }
