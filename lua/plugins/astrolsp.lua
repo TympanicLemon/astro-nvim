9a46c5959c878e307c3f7bdfaab262d9386b5bdc
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-05-18 04:36:37 -0700

    feat: added inlay hints & updated plugins

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     features = {
       -- NOTE: Change to true when neovim 0.10.0 is released on May 1st
-      inlay_hints = false,
+      inlay_hints = true,
     },
   },
 }
