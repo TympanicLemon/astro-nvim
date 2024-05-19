74efcad1663e3736dfbec7b9ed03762eeaaefa43
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-05-19 11:48:31 -0700

    added python stuff

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,7 @@ return {
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.project.project-nvim" },
 }
