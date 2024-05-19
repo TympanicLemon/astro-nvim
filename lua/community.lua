f02d5004fbb1823b0244cc33e644f8bea0471adf
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-05-19 08:22:34 -0700

    feat: added web dev support

--- lua/community.lua
+++ lua/community.lua
@@ -6,9 +6,11 @@ return {
   { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.utility.mason-tool-installer-nvim" },
 }
