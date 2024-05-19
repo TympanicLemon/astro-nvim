21625ac67ca8b2cc86eb63569daa9ae5d9aac1f6
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-05-19 11:03:30 -0700

    refactor: got rid of mason-tool and conform for now

--- lua/community.lua
+++ lua/community.lua
@@ -2,10 +2,10 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.project.project-nvim" },
-  -- { import = "astrocommunity.utility.mason-tool-installer-nvim" },
 }
