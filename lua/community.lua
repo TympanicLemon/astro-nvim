97283c5d10e4314d73505e5d2b792b6cbee91901
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-05-19 08:38:49 -0700

    task: get rid of all community packs, for now

--- lua/community.lua
+++ lua/community.lua
@@ -5,12 +5,7 @@ return {
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.utility.mason-tool-installer-nvim" },
 }
