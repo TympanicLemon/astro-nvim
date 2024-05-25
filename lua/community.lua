9c44a6d7ed1dae37de2cdd5ce9a5c371b5489907
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-05-24 21:12:32 -0700

    feat: added in html, css, and typescript support with project-nvim

--- lua/community.lua
+++ lua/community.lua
@@ -3,5 +3,8 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.project.project-nvim" },
 }
