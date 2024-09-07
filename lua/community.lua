94b12680efcf465ecba6b96dfb3f073746a9b51e
Daniel Zapata <danielz@Daniels-MBP-2.attlocal.net>
2024-09-07 00:00:56 -0700

    got rid of nvchad-statusline

--- lua/community.lua
+++ lua/community.lua
@@ -3,7 +3,6 @@ return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
     { import = "astrocommunity.motion.flash-nvim" },
-    { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
     { import = "astrocommunity.pack.lua" },
     { import = "astrocommunity.pack.python" },
     { import = "astrocommunity.pack.rust" },
