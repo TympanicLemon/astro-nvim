9c306dea0a7ba33d227aa40546f4af492386aa8c
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-09-16 16:25:08 -0700

    got rid of rust support, for now

--- lua/community.lua
+++ lua/community.lua
@@ -4,6 +4,5 @@ return {
     { import = "astrocommunity.motion.flash-nvim" },
     { import = "astrocommunity.pack.lua" },
     { import = "astrocommunity.pack.python" },
-    { import = "astrocommunity.pack.rust" },
     { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
 }
